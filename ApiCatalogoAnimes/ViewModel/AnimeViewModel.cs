6ab5810899e0d39846a79dab80bd52ff9cc6ed97
felipepellizzon <fapellizzon@gmail.com>
2021-10-13 20:50:08 -0300

    commit da parte 03 - 09:44 do projeto

---
+++ ApiCatalogoAnimes/ViewModel/AnimeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoAnimes.ViewModel
+{
+    public class AnimeViewModel
+    {
+    }
+}
